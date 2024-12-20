bc86ec0229ef2eb1e0174869c937c14b4dc83335
Luiz Mantuano <luizmantuano.dev@gmail.com>
2024-12-20 14:43:07 -0300

    feat: update PrivateStack to use typed screen names for navigation

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -2,13 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../pages/private/Home';
+import { PrivateStackParamList, Screens } from './types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<PrivateStackParamList>();
 
 const PrivateStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Home' component={Home} options={{ headerShown: false }}/>
+      <Stack.Screen name={Screens.Home} component={Home} options={{ headerShown: false }}/>
     </Stack.Navigator>
   )
 }
