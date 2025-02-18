d7cc6d7c5561b67901e0b93e6b81b4cb7bc027de
Luiz Mantuano <luizmantuano.dev@gmail.com>
2025-02-18 20:36:16 -0300

    feat: wrap app with ThemeProvider for styled-components theme support

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
-import Stack from './src/routes/stack';
-import { useFonts } from 'expo-font';
-
+import Stack from "./src/routes/stack";
+import { useFonts } from "expo-font";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
 
 export default function App() {
   const [fontLoaded] = useFonts({
-    'DMSans-Bold': require('./src/assets/fonts/DMSans-Bold.ttf'),
-    'DMSans-Medium': require('./src/assets/fonts/DMSans-Medium.ttf'),
-    'DMSans-Regular': require('./src/assets/fonts/DMSans-Regular.ttf'),
+    "DMSans-Bold": require("./src/assets/fonts/DMSans-Bold.ttf"),
+    "DMSans-Medium": require("./src/assets/fonts/DMSans-Medium.ttf"),
+    "DMSans-Regular": require("./src/assets/fonts/DMSans-Regular.ttf"),
   });
 
   if (!fontLoaded) {
     return null;
   }
   return (
-  <Stack />
-);
+    <ThemeProvider theme={theme}>
+      <Stack />
+    </ThemeProvider>
+  );
 }
