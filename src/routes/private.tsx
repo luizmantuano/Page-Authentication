d8b38f5d443cfefa59f13d80d4484be2aeef664b
Luiz Mantuano <luizmantuano.dev@gmail.com>
2025-02-24 10:26:08 -0300

    feat: replace Home screen with TabNavigator in PrivateStack

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -1,17 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../pages/private/Home';
-import { PrivateStackParamList, Screens } from './types';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { PrivateStackParamList, Screens } from "./types";
+import TabNavigator from "./tabs";
 
 const Stack = createNativeStackNavigator<PrivateStackParamList>();
 
 const PrivateStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name={Screens.Home} component={Home} options={{ headerShown: false }}/>
+      <Stack.Screen
+        name={Screens.Home}
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default PrivateStack
+export default PrivateStack;
