f80617fc51da08c6e9f63ea4328795c0cd08201d
Luiz Mantuano <luizmantuano.dev@gmail.com>
2025-03-10 19:48:05 -0300

    feat: enhance CustomInput component with email and password visibility features

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,29 +1,103 @@
-import React, { useState } from 'react';
-import { TextInput } from 'react-native';
-import { styles } from '../pages/public/Login/styles';
+import React, { useState } from "react";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import { Checkbox } from "react-native-paper";
+import { colors } from "../styles/colors";
+import { Ionicons } from "@expo/vector-icons";
 
-const CustomInput = ({ label, onChangeText, keyboardType, secureTextEntry = false, ...rest }) => {
-  const [focused, setFocused] = useState(false);
+interface CustomInputProps {
+  onChangeText: (text: string) => void;
+  keyboardType?: "default" | "email-address" | "numeric" | "phone-pad";
+  secureTextEntry?: boolean;
+  onPress?: () => void;
+  label: string;
+  status?: "checked" | "unchecked";
+  isEmail?: boolean;
+  isPassword?: boolean;
+  [key: string]: any;
+}
+
+const CustomInput = ({
+  onChangeText,
+  keyboardType,
+  label,
+  onPress,
+  status,
+  isEmail = false,
+  isPassword = false,
+  ...rest
+}: CustomInputProps) => {
+  const [hidepass, setHidepass] = useState(true);
 
   return (
-    <TextInput
-      style={[
-        styles.input,
-        {
-          borderColor: focused ? '#38A69D' : '#99f6e4', // Cor da borda baseada no foco
-          borderWidth: focused ? 1.5 : 1
-        }
-      ]}
-      placeholder={label}
-      onChangeText={onChangeText}
-      keyboardType={keyboardType}
-      secureTextEntry={secureTextEntry}
-      placeholderTextColor='#2C2948'
-      onFocus={() => setFocused(true)}
-      onBlur={() => setFocused(false)}
-      {...rest}
-    />
+    <View style={localStyles.container}>
+      <Text style={localStyles.textColor}>{label}</Text>
+      <View style={[localStyles.inputContainer]}>
+        <TextInput
+          style={localStyles.input}
+          onChangeText={onChangeText}
+          keyboardType={keyboardType}
+          secureTextEntry={hidepass}
+          placeholderTextColor="#2C2948"
+          {...rest}
+        />
+        {isEmail && (
+          <View style={localStyles.buttonCenter}>
+            <Checkbox
+              status={status}
+              onPress={onPress}
+              color={colors.primary500}
+            />
+          </View>
+        )}
+
+        {isPassword && (
+          <TouchableOpacity
+            style={localStyles.buttonCenter}
+            onPress={() => setHidepass(!hidepass)}
+          >
+            {hidepass ? (
+              <Ionicons name="eye-off" color={colors.grayScale6} size={22} />
+            ) : (
+              <Ionicons name="eye" color={colors.grayScale6} size={22} />
+            )}
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
   );
 };
+
+const localStyles = StyleSheet.create({
+  container: {
+    width: "90%",
+    marginBottom: 20,
+  },
+  buttonCenter: {
+    width: "15%",
+    alignItems: "center",
+  },
+  textColor: {
+    color: colors.primary500,
+    fontSize: 11,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: colors.grayScale6,
+    backgroundColor: "transparent",
+  },
+  input: {
+    flex: 1,
+    height: 30,
+    color: "#fff",
+  },
+});
 
 export default CustomInput;
